246971218ea65f28703dcd7cdf9d6445c4595368
osara22 <osaraiya22@gmail.com>
2021-10-24 16:38:29 +0900

    おみくじプログラム

---
+++ ConsoleApp/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var omikuji = random.Next(1, 5);
+            Console.WriteLine(omikuji);
+            if (omikuji == 1)
+            {
+                Console.WriteLine("大吉");
+            }
+            else if (omikuji == 2)
+            {
+                Console.WriteLine("中吉");
+            }
+            else if (omikuji == 3)
+            {
+                Console.WriteLine("小吉");
+            }
+            else
+            {
+                Console.WriteLine("凶");
+            }
+        }
+    }
+}
